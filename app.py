28f32759eb1c4709fabf64b60259a13b8f8272e7
Andy He <zhian.he@petronas.com>
2022-07-20 15:56:34 +0800

    add flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def test():
+   payload = request.get_json()
+
+   if payload:
+       print(f'Requested Json: {payload}')
+       return payload
+   else:
+       print('Request for test page received with json body')
+       return payload
+
+if __name__ == '__main__':
+   app.run()
